8a590c2697a7680e015b6a9160102e959f5dd6db
bissness11 <154408458+bissness11@users.noreply.github.com>
2024-08-20 16:24:37 +0500

    Update __init__.py

--- gdrive/__init__.py
+++ gdrive/__init__.py
@@ -14,28 +14,15 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 ENV = bool(os.environ.get('ENV', False))
 try:
   if ENV:
-    BOT_TOKEN = os.environ.get('BOT_TOKEN')
-    APP_ID = os.environ.get('APP_ID')
-    API_HASH = os.environ.get('API_HASH')
-    DATABASE_URL = os.environ.get('DATABASE_URL')
-    SUDO_USERS = os.environ.get('SUDO_USERS')
-    SUPPORT_CHAT_LINK = os.environ.get('SUPPORT_CHAT_LINK')
+    BOT_TOKEN = os.environ.get('BOT_TOKEN','5502855202:AAGW3MYUov5Gb2wWgpcaNTgxDjZ98IFh4P8')
+    APP_ID = os.environ.get('APP_ID','6534707')
+    API_HASH = os.environ.get('API_HASH','4bcc61d959a9f403b2f20149cbbe627a')
+    DATABASE_URL = os.environ.get('DATABASE_URL','postgres://dgdxlptb:uP0wjJhot4kqWrwXg8ENLEFAuEd2yk4d@mouse.db.elephantsql.com/dgdxlptb')
+    SUDO_USERS = os.environ.get('SUDO_USERS','1430593323')
+    SUPPORT_CHAT_LINK = os.environ.get('SUPPORT_CHAT_LINK','-1001714109958')
     DOWNLOAD_DIRECTORY = os.environ.get("DOWNLOAD_DIRECTORY", "./downloads/")
-    G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID")
-    G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET")
-  else:
-    from gdrive.config import config
-    BOT_TOKEN = config.BOT_TOKEN
-    APP_ID = config.APP_ID
-    API_HASH = config.API_HASH
-    DATABASE_URL = config.DATABASE_URL
-    SUDO_USERS = config.SUDO_USERS
-    SUPPORT_CHAT_LINK = config.SUPPORT_CHAT_LINK
-    DOWNLOAD_DIRECTORY = config.DOWNLOAD_DIRECTORY
-    G_DRIVE_CLIENT_ID = config.G_DRIVE_CLIENT_ID
-    G_DRIVE_CLIENT_SECRET = config.G_DRIVE_CLIENT_SECRET
-  SUDO_USERS = list({int(x) for x in SUDO_USERS.split()})
-  SUDO_USERS = list(set(SUDO_USERS))
+    G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID",'161719911468-5jtb7bsug2reg42nts4vmpah5r2l95e1.apps.googleusercontent.com ')
+    G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET",'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')
 except KeyError:
   LOGGER.error('One or more configuration values are missing exiting now.')
   sys.exit(1)
